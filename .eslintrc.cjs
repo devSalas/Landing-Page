9d7ea91d8f88cfed796a64aba8683fcd0eef3ef7
esteban salas <esalassulca@gmail.com>
2022-11-10 22:31:51 -0500

    proyecto iniciado, configuracion de eslint y prettier terminado

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommend',
+		'plugin:react/jsx-runtime',
+		'standard',
+		'eslint-config-prettier',
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		// 'no-unused-vars':'warn'
+	},
+}
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import './App.css'
+
+function App() {
+	return <div className={'app'}> <h1>Empecemos a programar! 🐱‍🏍</h1> </div>
+}
+
+export default App
