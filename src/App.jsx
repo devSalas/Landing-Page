c231ccadfef63448bc76b3ad0d8353ca2da187b8
esteban salas <esalassulca@gmail.com>
2022-11-14 21:13:18 -0500

    creacion de la estructuar de la pagina con grid personalizado de tailwind,terminado

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import Home from './pages/home'
 
 function App() {
 	return (
-		<div className="app max-w-7xl m-auto px-8">
-			<Home/>
+		<div className=" app max-w-7xl m-auto px-8 grid  grid-cols-layout  grid-rows-layout h-screen">
+
+			<div class="col-span-3 bg-green-500 sticky top-0 ">
+			</div>
+
+			<div class="scale-1 row-span-2 ">
+				
+				<div className='fixed top-16 bg-slate-500  w-16 h-screen'>
+				</div>
+
+			</div>
+
+			<div class="row-span-2 col-span-2 pb-72"> <Home/></div>
+			 
 		</div>
 	)
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        8: 'repeat(8, minmax(0, 1fr))',
+        layout: '64px 1fr',
+      },
+      gridTemplateColumns:{
+        layout:'64px 1fr'
+      },
+      colors:{
+        blackp:'#fdee'
+      }
+    },
   },
   plugins: [],
 }
