913d83745072a9116b16cb553519e74691baa8fa
Gerson Garayar <garayargerson20@gmail.com>
2022-11-17 14:46:33 -0500

    modicando seccion de video

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -22,14 +22,13 @@ const Home = () => {
 
           <article className="flex overflow-x-scroll flex-col flex-wrap h-72 gap-4">
             <VideoCard title="Street work" video={"videos/001.mp4"} user="Mr.Rick Tomson" image={"https://randomuser.me/api/portraits/men/27.jpg"} />
-            <VideoCard title="Street work" video={"videos/002.mp4"} user="Bob gamer450" image={"https://randomuser.me/api/portraits/men/81.jpg"}/>
+            <VideoCard title="Jugando GTA" video={"videos/002.mp4"} user="Bob gamer450" image={"https://randomuser.me/api/portraits/men/81.jpg"}/>
             <VideoCard title="Street work" video={"videos/003.mp4"} user="Milena Foster" image={"https://randomuser.me/api/portraits/women/63.jpg"}/>
             <VideoCard title="Street work" video={"videos/004.mp4"} user="Mr.Rick Tomson" image={"https://randomuser.me/api/portraits/men/27.jpg"}/>
             <VideoCard title="Street work" video={"videos/005.mp4"} user="Alex123" image={"https://randomuser.me/api/portraits/men/73.jpg"}/>
             
           </article>
             
- 
         </section>
       </main>
     </>
