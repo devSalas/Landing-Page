b26e69db620ebea35a1a1e8a4e7e208c04b1961d
esteban salas <esalassulca@gmail.com>
2022-12-04 12:21:54 -0500

    fix,ruta de las imagenes

--- src/components/TabBar.jsx
+++ src/components/TabBar.jsx
@@ -6,21 +6,16 @@ export default function Toobar() {
 			<div className='bg-black w-full fixed bottom-0 md:top-0 left-0 md:w-16 md:h-screen flex md:flex-col justify-center md:justify-start md:pt-16 items-center '>
 			
 			<Link href='/' >
-				<img className='p-4 cursor-pointer' src="../../public
-/home.svg" alt="" />
+				<img className='p-4 cursor-pointer' src="./home.svg" alt="" />
 			</Link>
 
-				<img className='p-4 cursor-pointer' src="../../public
-/plus-square.svg" alt="" />
+				<img className='p-4 cursor-pointer' src="./plus-square.svg" alt="" />
 
-				<img className='p-4 cursor-pointer' src="../../public
-/folder.svg" alt="" />
+				<img className='p-4 cursor-pointer' src="./folder.svg" alt="" />
 
-				<img className='p-4 cursor-pointer' src="../../public
-/star.svg" alt="" />
+				<img className='p-4 cursor-pointer' src="./star.svg" alt="" />
 
-				<img className='p-4 cursor-pointer' src="../../public
-/settings.svg" alt="" />
+				<img className='p-4 cursor-pointer' src="./settings.svg" alt="" />
 
 			</div>
 
